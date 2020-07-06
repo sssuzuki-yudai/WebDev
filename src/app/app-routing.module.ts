7c1194eeb172202727b19af67a56b92e184911b8
Yudai <things.without.color@gmail.com>
2020-07-07 08:49:26 +0900

    product module 作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductModule } from './product/product.module';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 
 const routes: Routes = [
-   { path: '', redirectTo: 'products', pathMatch: 'full' },
-  // { path: 'detail', component: ProductDetailComponent}
+    { path: '', component: ProductListComponent },
+    { path: 'detail', component: ProductDetailComponent }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-    ProductModule
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
