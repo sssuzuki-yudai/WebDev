d4a87eb5aa6a3f2eb004fb43b62954c7a38ff9f5
Yudai <things.without.color@gmail.com>
2020-07-10 08:05:04 +0900

    Nodeサーバー構築

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,8 +1,24 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const Product = require('../mosdel/product');
 
 router.get('', function(req, res) {
-    res.json({ 'ok': true })
+    Product.find({}, function(err, foundProducts) {
+        return res.json(foundProducts)
+    })
+    // res.json({ 'ok': true })
 })
+
+router.get('/:productId', function(req, res) {
+    const productId = req.params.productId
+    Product.findById(productId, function(err, foundProduct) {
+        if(err) {
+            return res.status(422).send({errors: [{title: 'Product error', detail: 'Product not found'}]})
+        }
+        return res.json(foundProduct)
+    })
+    // res.json({ 'ok': true })
+})
+
 
 module.exports = router
